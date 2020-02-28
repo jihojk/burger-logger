23fa17b8ae093a2ede7cc9977b388ab3bf39cf6a
Jiho Kim <jihojk@uci.edu>
2020-02-27 19:00:37 -0800

    all bugs fixed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,44 +3,30 @@ var router = express.Router();
 
 var burgers = require("../models/burgers.js");
 
-router.get("/", function (req, res) {
-
+router.get("/", function(req, res) {
   res.redirect("/index");
-
 });
 
-router.get("/index", function (req, res) {
-
-  burgers.all(function (data) {
-
+router.get("/index", function(req, res) {
+  burgers.all(function(data) {
     var hbrObj = {
       burgers: data
     };
 
     res.render("index", hbrObj);
-  
   });
-
 });
 
-router.post("/burgers/create", function (req, res) {
-
-  burgers.create(req.body.burger_name, function (result) {
-
+router.post("/burgers/create", function(req, res) {
+  burgers.create(req.body.burger_name, function(result) {
     res.redirect("/index");
-
   });
-
 });
 
-router.post("/burgers/eat/:id", function (req, res) {
-
-  burgers.update(req.params.id, function (result) {
-
+router.post("/burgers/eat/:id", function(req, res) {
+  burgers.update(req.params.id, function(result) {
     res.redirect("/index");
-
   });
-
 });
 
 module.exports = router;
